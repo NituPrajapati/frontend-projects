6a8fc41da4d7e7f58adcdd1ea897702c7ffe71b7
NituPrajapati <prajapatinitu3425@gmail.com>
2025-07-16 09:18:54 -0700

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import './App.css'
-import Todo from './Todo/Todo'
+import Navbar from "./components/Navbar"
+import Banner from "./components/Banner"
+import Banner2 from "./components/Banner2"
+import Banner3 from "./components/Banner3"
+import Banner4 from "./components/Banner4"
+import Banner5 from "./components/Banner5"
+import Footer from "./components/Footer"
 function App() {
+
   return (
     <>
-      <Todo />
+    <Navbar />
+    <Banner />
+    <Banner2 />
+    <Banner4 />
+    <Banner3 /> 
+    <Banner5 />
+    <Footer />
     </>
   )
 }
